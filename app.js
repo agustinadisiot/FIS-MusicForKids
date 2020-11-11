3e84cbb997d230103adabc30a5e34a128bc6f653
Agustina <agustinadisiotvevo@gmail.com>
2020-11-11 18:59:56 -0300

    Agregue webpack siguiendo la guia de Google

---
+++ app.js
@@ -0,0 +1,6 @@
+import {MDCRipple} from '@material/ripple/index';
+import {MDCSlider} from '@material/slider';
+
+const slider = new MDCSlider(document.querySelector('.mdc-slider'));
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const ripple2 = new MDCRipple(document.querySelector('.foo-button2'));
