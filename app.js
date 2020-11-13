c3b9d6b2bcda359e2b9d453934eb5f8c51289c5e
Joaquin Meerhoff <jm247096@fi365.ort.edu.uy>
2020-11-13 18:56:52 -0300

    Agregue funcion con html para Secciones

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+/*jshint esversion: 6 */
 import {MDCRipple} from '@material/ripple/index';
 import {MDCSlider} from '@material/slider';
 
 const slider = new MDCSlider(document.querySelector('.mdc-slider'));
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
-const ripple2 = new MDCRipple(document.querySelector('.foo-button2'));
+const ripple3 = new MDCRipple(document.querySelector('.foo-button3'));
